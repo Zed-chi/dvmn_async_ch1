ff21843f430262dd09b2e9e70d6359056971ab71
zed <zed@mail.ru>
2020-12-29 00:10:52 +0300

    black

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import time
 import asyncio
 import curses
 
-from garbage import get_trash_coroutines, fill_orbit_with_garbage, get_trash_frames, fly_garbage
+from garbage import (
+    get_trash_coroutines,
+    fill_orbit_with_garbage,
+    get_trash_frames,
+    fly_garbage,
+)
 from stars import get_stars_coroutines
 from rocket import draw_rocket, fire
-
-
-
 
 
 def draw(canvas):
@@ -19,18 +21,20 @@ def draw(canvas):
     border = {"top": 0, "bottom": height, "left": 0, "right": width}
     stars_coroutines = get_stars_coroutines(canvas, width, height)
     rocket_coroutine = draw_rocket(
-        canvas, height / 2, width / 2, border, speed_boost=1
+        canvas, height // 2, width // 2, border, speed_boost=1
     )
     trash_cors = get_trash_coroutines(canvas, width, height, 6)
-    fire_coroutine = fire(canvas, height - 1, width / 2)
-    #filler = fill_orbit_with_garbage(routines, canvas, width)
+    fire_coroutine = fire(canvas, height - 1, width // 2)
+    filler = fill_orbit_with_garbage(canvas, width, routines)
+
     routines = [
-        *stars_coroutines,
+        filler,
         rocket_coroutine,
         fire_coroutine,
+        *stars_coroutines,
         *trash_cors,
-        
     ]
+
     while True:
         for coroutine in routines.copy():
             try:
