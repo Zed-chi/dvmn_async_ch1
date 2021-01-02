3a76bc39b259849210eb926b134765f0eb9185be
zed <zed@mail.ru>
2021-01-02 23:35:58 +0300

    added end titles

--- rocket.py
+++ rocket.py
@@ -3,11 +3,25 @@ import curses
 from curses_tools import read_controls, draw_frame, get_frame_size
 from utils import sleep
 from physics import update_speed
-from obstacles import Collision
+from obstacles import Collision, Obstacle
+from garbage import fly_garbage
 
 """ rocket stuff """
 
+def get_end():
+    with open("end.txt", "r") as file:
+        return file.read()
 
+async def end(canvas, row, column):    
+    rows_number, columns_number = canvas.getmaxyx()
+
+    column = max(column, 0)
+    column = min(column, columns_number - 1)
+
+    while row < rows_number:        
+        draw_frame(canvas, row, column, get_end())        
+        await sleep(1) 
+    
 def get_rocket_frames_iter():
     with open(
         "./animations/rocket_frame_1.txt", "r", encoding="utf-8"
@@ -19,12 +33,6 @@ def get_rocket_frames_iter():
         frame_2 = file2.read()
 
     return cycle([frame_1, frame_1, frame_2, frame_2])
-
-
-async def run_spaceship(
-    routines,
-):
-    pass
 
 
 async def fire(collisions,
@@ -76,14 +84,24 @@ async def draw_rocket(
     border,
     negative=True,
     speed_boost=0,
-):
+):      
     row, column = (start_row, start_column)
     row_speed = column_speed = 0
     frames = get_rocket_frames_iter()
+    rows, columns = get_frame_size(next(frames))
+    rocket = Obstacle(row, column, rows, columns)
+    
     for frame in frames:
+        rocket.row = row
+        rocket.column = column
+        for i in obstacles:
+            if rocket.has_collision(i.row, i.column, i.rows_size, i.columns_size):
+                routines.append(end(canvas, row, column))
+                return
         draw_frame(canvas, row, column, frame)
         await sleep()
         draw_frame(canvas, row, column, frame, negative=True)
+
 
         row_delta, column_delta, space = read_controls(canvas)
         frame_rows, frame_columns = get_frame_size(frame)
@@ -117,3 +135,4 @@ async def draw_rocket(
             routines.append(
                 fire(collisions, obstacles, canvas, row, column)
             )
+        
